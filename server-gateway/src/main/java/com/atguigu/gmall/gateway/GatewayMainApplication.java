91e50163932dfe1cb94dfca405b8baab7e580dba
darkTracy <1173844043@qq.com>
2022-08-23 16:40:12 +0800

    Initial commit

---
+++ server-gateway/src/main/java/com/atguigu/gmall/gateway/GatewayMainApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringBootApplication
+@SpringCloudApplication
+public class GatewayMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMainApplication.class,args);
+    }
+}
