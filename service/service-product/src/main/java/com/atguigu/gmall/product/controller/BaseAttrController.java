3990b3c117f52a7258a596589823457de2df37a7
darkTracy <1173844043@qq.com>
2022-08-24 23:31:04 +0800

    刷新数据2

--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/BaseAttrController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/BaseAttrController.java
@@ -1,19 +1,32 @@
 package com.atguigu.gmall.product.controller;
 
 import com.atguigu.gmall.common.result.Result;
+import com.atguigu.gmall.model.product.BaseAttrInfo;
+import com.atguigu.gmall.product.service.BaseAttrInfoService;
+import com.atguigu.gmall.product.service.BaseAttrValueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("admin/product")
 public class BaseAttrController {
-    @Autowired
+    @Autowired(required = false)
+    BaseAttrInfoService baseAttrInfoService;
+
+    @Autowired(required = false)
+    BaseAttrValueService baseAttrValueService;
+
     @GetMapping("attrInfoList/{category1Id}/{category2Id}/{category3Id}")
+    public Result getAttrInfoList(@PathVariable("category1Id")Long category1Id,
+                                  @PathVariable("category2Id")Long category2Id,
+                                  @PathVariable("category3Id")Long category3Id){
 
-
-    public Result attrInfoList(){
-
+        List<BaseAttrInfo> infos = baseAttrInfoService.getAttrInfoAndValueByCategoryId(category1Id,category2Id,category3Id);
+        return Result.ok(infos);
     }
 }
