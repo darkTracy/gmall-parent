384421edccabed7acca73d0798c790537a49362a
darkTracy <1173844043@qq.com>
2022-09-02 20:29:02 +0800

    更新4

--- service/web-all/src/main/java/com/atguigu/gmall/web/WebAllMainApplication.java
+++ service/web-all/src/main/java/com/atguigu/gmall/web/WebAllMainApplication.java
@@ -2,11 +2,7 @@ package com.atguigu.gmall.web;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
@@ -20,12 +16,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 //@EnableDiscoveryClient
 //@EnableCircuitBreaker
-
-@EnableFeignClients
+//
+@EnableFeignClients(basePackages = {
+        "com.atguigu.gmall.feign.item",
+        "com.atguigu.gmall.feign.product"
+}) //只会扫描主程序所在的子包
 @SpringCloudApplication
 public class WebAllMainApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(WebAllMainApplication.class,args);
     }
